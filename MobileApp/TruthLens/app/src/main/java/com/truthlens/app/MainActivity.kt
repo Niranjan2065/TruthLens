3911dd5216e7381d6b15afd0b917d44a0be9ea36
Niranjan2065 <gniranjan2065@gmail.com>
2026-08-24 22:30:38 +0530

    add complete AI training and TensorFlow Lite pipeline

---
+++ MobileApp/TruthLens/app/src/main/java/com/truthlens/app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.truthlens.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.truthlens.app.ui.theme.TruthLensTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TruthLensTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TruthLensTheme {
+        Greeting("Android")
+    }
+}
---
+++ MobileApp/TruthLens/app/src/main/java/com/truthlens/app/TruthLensModel.kt
@@ -0,0 +1,4 @@
+package com.truthlens.app
+
+class TruthLensModel {
+}
